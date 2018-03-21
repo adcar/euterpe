bfd8298dcb7fd52935de19cb88ae7fd92b707850
adcar <13227184+adcar@users.noreply.github.com>
2018-03-21 10:49:26 -0400

    Added detection for prod or dev

--- src/getToken.js
+++ src/getToken.js
@@ -1,12 +1,18 @@
 import Cookie from 'js-cookie'
+console.log(process.env)
+let url = ''
+if (process.env.NODE_ENV === 'production') {
+	url = 'https://apollomusic.tk'
+} else if (process.env.NODE_ENV === 'development') {
+	url = 'http://localhost:3000'
+}
 
 const getToken = token => {
 	let cookie = Cookie.get(token)
 	if (cookie) {
 		return cookie
 	} else {
-		window.location =
-			'https://accounts.spotify.com:443/authorize?client_id=88ed4852708440e6908246065f5a7ca4&response_type=token&redirect_uri=http://localhost:3000/callback&scope=user-library-modify%20user-library-read%20user-top-read'
+		window.location = `https://accounts.spotify.com:443/authorize?client_id=88ed4852708440e6908246065f5a7ca4&response_type=token&redirect_uri=${url}/callback&scope=user-library-modify%20user-library-read%20user-top-read`
 	}
 }
 
