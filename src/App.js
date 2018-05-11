0395609b5eb5a76c2e85cbd1475aa3d0e025ad96
adcar <adcar@github.com>
2018-05-10 21:11:10 -0400

    Integrated with redux-persist (Will for allow API token inside Redux)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import thunk from 'redux-thunk'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 import rootReducer from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import storage from 'redux-persist/lib/storage'
 
 import Collection from './routes/Collection'
 import Artist from './routes/Artist'
@@ -24,10 +28,21 @@ import Search from './routes/Search'
 import blue from 'material-ui/colors/blue'
 import amber from 'material-ui/colors/amber'
 
+// Redux configuration
+const persistConfig = {
+	key: 'root',
+	storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = createStore(
-	rootReducer,
+	persistedReducer,
 	composeWithDevTools(applyMiddleware(thunk))
 )
+
+const persistor = persistStore(store)
+
 const theme = createMuiTheme({
 	palette: {
 		type: 'light',
@@ -43,29 +58,31 @@ class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<MuiThemeProvider theme={theme}>
-					<CssBaseline />
-					<Router>
-						<div>
-							<Navbar>
-								<Route
-									exact
-									path="/"
-									render={() => <Redirect to="/browse" />}
-								/>
-								<Route path="/browse" component={Home} />
-								<Route path="/callback" component={Callback} />
-								<Route path="/artist/:id" component={Artist} />
-								<Route path="/collection" component={Collection} />
-								<Route path="/album/:id" component={Album} />
-								<Route path="/playlist/:user/:id" component={Playlist} />
-								<Route path="/category/:id" component={Category} />
-								<Route path="/search" component={Search} />
-							</Navbar>
-							<PlaylistPlayer />
-						</div>
-					</Router>
-				</MuiThemeProvider>
+				<PersistGate loading={null} persistor={persistor}>
+					<MuiThemeProvider theme={theme}>
+						<CssBaseline />
+						<Router>
+							<div>
+								<Navbar>
+									<Route
+										exact
+										path="/"
+										render={() => <Redirect to="/browse" />}
+									/>
+									<Route path="/browse" component={Home} />
+									<Route path="/callback" component={Callback} />
+									<Route path="/artist/:id" component={Artist} />
+									<Route path="/collection" component={Collection} />
+									<Route path="/album/:id" component={Album} />
+									<Route path="/playlist/:user/:id" component={Playlist} />
+									<Route path="/category/:id" component={Category} />
+									<Route path="/search" component={Search} />
+								</Navbar>
+								<PlaylistPlayer />
+							</div>
+						</Router>
+					</MuiThemeProvider>
+				</PersistGate>
 			</Provider>
 		)
 	}
